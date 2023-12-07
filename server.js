0aec010325fd10a8a029920ee0eea055b97d618c
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-07 14:39:21 -0500

    moved db contents to database

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
-// const database = require("./db");
+const database = require("./database");
 
-let db_con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ''
-});
+// let db_con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: ''
+// });
 
-db_con.connect((err) => {
-    if (err) {
-      console.log("Database Connection Failed !!!", err);
-    } else {
-      console.log("connected to Database");
-    }
-});
+// db_con.connect((err) => {
+//     if (err) {
+//       console.log("Database Connection Failed !!!", err);
+//     } else {
+//       console.log("connected to Database");
+//     }
+// });
   
-module.exports = db_con;
+// module.exports = db_con;
 
 // const database = require('./sqlConnection');
 
