5f537f9fcb6118ba49824f80c8eb2d0b6df2b0f7
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-07 20:05:55 -0500

    adding seeds

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,5 @@
+insert into users (email, dob) values ('katherine@lighthouse.com', '1992-10-21');
+insert into users (email) values ('jamie@lighthouse.com');
+insert into users (email) values ('joshon@lighthouse.com');
+insert into users (email) values ('clara@lighthouse.com');
+insert into users (email) values ('susan@lighthouse.com');
