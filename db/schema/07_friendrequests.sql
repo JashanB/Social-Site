2c2f4460cfbe16e975a564480025f82be054731b
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-07 19:55:45 -0500

    updating db schema

--- db/schema/07_friendrequests.sql
+++ db/schema/07_friendrequests.sql
@@ -2,10 +2,12 @@ DROP TABLE IF EXISTS friendRequests CASCADE;
 
 CREATE TABLE friendRequests (
     id SERIAL PRIMARY KEY NOT NULL,
-    from_user_id SMALLINT,
-    to_user_id SMALLINT,
-    status VARCHAR(20) DEFAULT 'pending',
+    from_user_id SMALLINT NOT NULL,
+    to_user_id SMALLINT NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending' NOT NULL,
     requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (from_user_id) REFERENCES users(id),
-    FOREIGN KEY (to_user_id) REFERENCES users(id)
+    FOREIGN KEY (to_user_id) REFERENCES users(id),
+    from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    to_user_id INTEGER REFERENCES posts(id) ON DELETE CASCADE
 );
