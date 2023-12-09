0249fd377e0e8c1b5f83539fa21f1f5d3283b9f9
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-09 14:55:38 -0500

    server running

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const database = require("./database");
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const ENV = process.env.ENV || "development";
+// const database = require("./database");
+// const morgan = require('morgan');
+// const methodOverride = require('method-override');
+// const ENV = process.env.ENV || "development";
 
 app.use(cors());
 app.use(express.json());
