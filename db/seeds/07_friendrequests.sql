98a3f0f1b914b5b97aa9fa4ea90d84523d446ec5
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-08 19:18:51 -0500

    adding seeds

--- db/seeds/07_friendrequests.sql
+++ db/seeds/07_friendrequests.sql
@@ -1,13 +1,5 @@
-DROP TABLE IF EXISTS friendRequests CASCADE;
-
-CREATE TABLE friendRequests (
-    id SERIAL PRIMARY KEY NOT NULL,
-    from_user_id SMALLINT NOT NULL,
-    to_user_id SMALLINT NOT NULL,
-    status VARCHAR(20) DEFAULT 'pending' NOT NULL,
-    requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (from_user_id) REFERENCES users(id),
-    FOREIGN KEY (to_user_id) REFERENCES users(id),
-    from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    to_user_id INTEGER REFERENCES posts(id) ON DELETE CASCADE
-);
+insert into friendRequests (from_user_id, to_user_id) values (1, 2);
+insert into friendRequests (from_user_id, to_user_id) values (1, 3);
+insert into friendRequests (from_user_id, to_user_id) values (2, 3);
+insert into friendRequests (from_user_id, to_user_id) values (4, 3);
+insert into friendRequests (from_user_id, to_user_id) values (5, 1);
