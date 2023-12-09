de9d461561ab55139abb0f388b836b58385c46b1
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-09 16:15:17 -0500

    connected db to server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,31 @@ const app = express();
 // const morgan = require('morgan');
 // const methodOverride = require('method-override');
 // const ENV = process.env.ENV || "development";
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+ 
+const connectDb = async () => {
+    try {
+        const pool = new Pool({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT,
+        });
+        await pool.connect()
+        const res = await pool.query('SELECT * FROM users')
+        console.log(res.rows)
+        await pool.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+
+connectDb()
+
 
 app.use(cors());
 app.use(express.json());
