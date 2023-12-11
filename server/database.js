6f55faf8754f01c75b7e9ef47ffd4c93a8f45dcb
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-11 15:29:57 -0500

    adding db functions

--- server/database.js
+++ server/database.js
@@ -1,4 +1,39 @@
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
+// const { Pool } = require('pg');
+// const dbParams = require('./lib/db.js');
+// const db = new Pool(dbParams);
+// db.connect();
+
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+const dbParams = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+dotenv.config();
+const connectDb = async () => {
+    try {
+        await dbParams.connect()
+        // await dbParams.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
+connectDb()
+
+async function getAllUsers () {
+    const res = await dbParams.query('SELECT * FROM users');
+    // console.log(res.rows)
+    return res.rows;
+}
+
+exports.getAllUsers = getAllUsers;
+
+// getAllUsers();
+
+// function getUserWIthEmail (email) {
+
+// }
