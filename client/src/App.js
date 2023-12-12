f053468d1db0d84939138340f07c313ee5158ac3
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-12 15:36:57 -0500

    adding get requests

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,27 @@ import axios from 'axios';
 
 function App() {
   const [message, setMessage] = useState("");
-
+  const [email, setEmail] = useState("clara@lighthouse.com")
+  // console.log(message)
   useEffect(() => {
     // axios.get("http://localhost:8000/message")
     // .then((response) => setMessage(state => response.data.message));
-    axios.get("http://localhost:8000/message", )
-    .then((response) => setMessage(state => response.data.message));
+    axios.get("http://localhost:8000/api")
+      .then((response) => {
+        console.log('this', response)
+        // if (response.data.message) setMessage(state => response.data.message)
+        setMessage(state => response.data.message);
+      });
+    axios.get(`http://localhost:8000/api/users/${email}`)
+      .then((response) => {
+        console.log('that', response)
+        // if (response.data.message) setMessage(state => response.data.message)
+        // setMessage(state => response.data.message)
+      });
   }, []);
 
   return (
     <div className="App">
-      <h1>{message}</h1>
     </div>
   );
 }
