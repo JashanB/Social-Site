3794746eb7cfb9b73b01f97699cd70a96fd72b36
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-13 13:06:05 -0500

    adding login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -2,11 +2,38 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './Login.css';
 
-export default (props) => {
-    
+export default () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onLogin({ username, password });
+    }
+
     return (
-      <div className={"login-page"}>
-        
-      </div>
+        <div className={"login-page"}>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
     )
-  }
+}
