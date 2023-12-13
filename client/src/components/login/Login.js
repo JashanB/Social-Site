f3b0a2830500b7f352b5740f5a48ae36cb611f5a
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-13 13:04:57 -0500

    adding login

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,2 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './Login.css';
 
+export default (props) => {
+    
+    return (
+      <div className={"login-page"}>
+        
+      </div>
+    )
+  }
