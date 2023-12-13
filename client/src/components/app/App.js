50b96adb2baafc0f7fbdd91b8ac226b1fbf5dcc2
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-13 14:14:26 -0500

    cleaning up test code

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -19,12 +19,12 @@ function App() {
   useEffect(() => {
     // axios.get("http://localhost:8000/message")
     // .then((response) => setMessage(state => response.data.message));
-    axios.get("http://localhost:8000/api")
-      .then((response) => {
-        console.log('this', response)
-        // if (response.data.message) setMessage(state => response.data.message)
-        setMessage(state => response.data.message);
-      });
+    // axios.get("http://localhost:8000/api")
+    //   .then((response) => {
+    //     console.log('this', response)
+    //     // if (response.data.message) setMessage(state => response.data.message)
+    //     setMessage(state => response.data.message);
+    //   });
     axios.get(`http://localhost:8000/api/users/${email}`)
       .then((response) => {
         console.log('that', response)
