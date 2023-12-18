f594ee8d529ff1ea6cb39c9118e561a7a8bf441d
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-18 10:45:49 -0500

    adding profile

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import './Profile.css';
+
+function Profile (props) {
+
+    return (
+        <div className={"profile-page"}>
+        </div>
+    )
+}
+
+export default Profile;
