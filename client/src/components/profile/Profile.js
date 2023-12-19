1cd52050da030837559ad3e75c671e6b240866df
Jashan Brar <jashan_brar50@hotmail.com>
2023-12-19 13:00:58 -0500

    planning profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,7 +4,11 @@ import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import './Profile.css';
 
 function Profile (props) {
-
+    //have option on profile to show either posts (default), liked, saved
+    //make state to show each of them on demand
+    //make axios get request to get data for each of them
+    //either make req for posts first, then the other 2 after (save as object + spread), or request for all 3 initially, or request only on clicking each
+    //lazy load images - will be using on home page too
     return (
         <div className={"profile-page"}>
         </div>
