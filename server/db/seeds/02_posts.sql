304cb06a27fa0954602c3dd7a409e30c860aa178
Jashan Brar <jashan_brar50@hotmail.com>
2024-01-30 15:18:28 -0500

    added imgs

--- server/db/seeds/02_posts.sql
+++ server/db/seeds/02_posts.sql
@@ -1,5 +1,15 @@
-insert into posts (title, content, img, user_id) values ('Check out my new bag!', 'blank', 'cool_image.png', 1);
 insert into posts (title, content, img, user_id) values ('Check out my new toy!', 'cool right', 'cool_image.png', 2);
 insert into posts (title, content, img, user_id) values ('Check out my new scarf!', 'blank', 'cool_image.png', 3);
 insert into posts (title, content, img, user_id) values ('Check out my new shirt!', 'thrifted', 'cool_image.png', 4);
 insert into posts (title, content, img, user_id) values ('Check out my new mug!', 'Yay', 'cool_image.png', 5);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img1.jpg', 'img1-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img2.jpg', 'img2-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img3.jpg', 'img3-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img4.jpg', 'img4-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img5.jpg', 'img5-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img6.jpg', 'img6-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img7.jpg', 'img7-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img8.jpg', 'img8-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img9.jpg', 'img9-small.jpg', 1);
+insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img20.jpg', 'img10-small.jpg', 1);
+
