d171e0483bdecea7faa065cd5c185b5de9d20ded
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-04 21:32:12 -0500

    updating seeds

--- server/db/seeds/06_friends.sql
+++ server/db/seeds/06_friends.sql
@@ -1,5 +1,9 @@
-insert into friends (user_id_1, user_id_2) values (1, 2);
-insert into friends (user_id_1, user_id_2) values (1, 3);
-insert into friends (user_id_1, user_id_2) values (2, 3);
-insert into friends (user_id_1, user_id_2) values (4, 3);
-insert into friends (user_id_1, user_id_2) values (5, 1);
+insert into friends (user_id, friends_id) values (1, 2);
+insert into friends (user_id, friends_id) values (2, 1);
+insert into friends (user_id, friends_id) values (1, 3);
+insert into friends (user_id, friends_id) values (3, 1);
+insert into friends (user_id, friends_id) values (2, 3);
+insert into friends (user_id, friends_id) values (3, 2);
+insert into friends (user_id, friends_id) values (4, 1);
+insert into friends (user_id, friends_id) values (1, 4);
+
