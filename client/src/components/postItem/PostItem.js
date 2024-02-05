aa9a892083b29e7dd87d21f5a7f9975601252b51
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-05 17:00:41 -0500

    adding post item

--- client/src/components/postItem/PostItem.js
+++ client/src/components/postItem/PostItem.js
@@ -5,7 +5,9 @@ import './PostItem.css';
 function PostItem (props) {
 
     return (
-        <div className="post-item">
+        <div className="post-item" key={props.title}>
+            <h2>{props.title}</h2>
+            <img src={require(`../../../../imgs/${props.img}`)}></img>
         </div>
     )
 }
