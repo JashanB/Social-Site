cfc1f662564cb9db35a0c4df55ce9a5855f175b7
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-05 13:01:39 -0500

    fixed table

--- server/db/schema/07_friendrequests.sql
+++ server/db/schema/07_friendrequests.sql
@@ -6,8 +6,6 @@ CREATE TABLE friendRequests (
     to_user_id SMALLINT NOT NULL,
     status VARCHAR(20) DEFAULT 'pending' NOT NULL,
     requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (from_user_id) REFERENCES users(id),
-    FOREIGN KEY (to_user_id) REFERENCES users(id),
-    from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    to_user_id INTEGER REFERENCES posts(id) ON DELETE CASCADE
+    FOREIGN KEY (from_user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_user_id) REFERENCES users(id) ON DELETE CASCADE
 );
