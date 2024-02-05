1b8ede3bef138a1e8a429f33e4d070f5f277f2eb
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-05 13:00:36 -0500

    updating seeds

--- server/db/seeds/02_posts.sql
+++ server/db/seeds/02_posts.sql
@@ -1,7 +1,7 @@
-insert into posts (title, content, img, user_id) values ('Check out my new toy!', 'cool right', 'cool_image.png', 2);
-insert into posts (title, content, img, user_id) values ('Check out my new scarf!', 'blank', 'cool_image.png', 3);
-insert into posts (title, content, img, user_id) values ('Check out my new shirt!', 'thrifted', 'cool_image.png', 4);
-insert into posts (title, content, img, user_id) values ('Check out my new mug!', 'Yay', 'cool_image.png', 5);
+-- insert into posts (title, content, img, user_id) values ('Check out my new toy!', 'cool right', 'cool_image.png', 2);
+-- insert into posts (title, content, img, user_id) values ('Check out my new scarf!', 'blank', 'cool_image.png', 3);
+-- insert into posts (title, content, img, user_id) values ('Check out my new shirt!', 'thrifted', 'cool_image.png', 4);
+-- insert into posts (title, content, img, user_id) values ('Check out my new mug!', 'Yay', 'cool_image.png', 5);
 insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img1.jpg', 'img1-small.jpg', 1);
 insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img2.jpg', 'img2-small.jpg', 1);
 insert into posts (title, content, img, img_small, user_id) values ('Oh look at this bird', 'blank', 'img3.jpg', 'img3-small.jpg', 1);
