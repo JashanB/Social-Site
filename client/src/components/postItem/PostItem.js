0d77a54017aa62f4e468e4dd3a02c5776d35b82f
Jashan Brar <jashan_brar50@hotmail.com>
2024-02-05 19:49:19 -0500

    adding lazy load

--- client/src/components/postItem/PostItem.js
+++ client/src/components/postItem/PostItem.js
@@ -8,7 +8,7 @@ function PostItem(props) {
         <div className="post-item" key={props.title}>
             <h2>{props.title}</h2>
             {/* <img src={require(`../../../../imgs/${props.img}`)}></img> */}
-            <div classname="blur-load" style={{ backgroundImage: `url(../../../../imgs/${props.img_small})` }}>
+            <div className="blur-load" style={{ backgroundImage: `url(../../../../imgs/${props.img_small})` }}>
                 <img src={require(`../../../../imgs/${props.img}`)} loading="lazy" />
             </div>
         </div>
